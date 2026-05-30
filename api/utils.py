c9f9a108a0c3e1cbe9dd3231d493e70705f36b4b
didamendes <didamendes@hotmail.com>
2026-05-30 11:29:57 -0300

    Refactor `common_api_token` to move `API_TOKEN` initialization into the function

--- api/utils.py
+++ api/utils.py
@@ -5,12 +5,11 @@ from groq import Groq
 
 from api.models import Historia
 
-API_TOKEN = (str(os.getenv("API_TOKEN")),)
-
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 
 def common_api_token(api_token: str):
+    API_TOKEN = (str(os.getenv("API_TOKEN")),)
     if api_token != API_TOKEN:
         raise fastapi.HTTPException(
             status_code=fastapi.status.HTTP_401_UNAUTHORIZED, detail="Token inválido"
